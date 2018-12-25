0c2274b77acdf0064c78771b65616f722ab29661
Prakhar <prakhar_garg@comcast.com>
2018-12-24 23:17:10 -0500

    formatting photo

--- background.js
+++ background.js
@@ -8,19 +8,23 @@ window.onload = function() {
     startPos = position;
     // document.getElementById('startLat').innerHTML = startPos.coords.latitude;
     // document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-    fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + startPos.coords.latitude + ',' + startPos.coords.longitude + '&radius=1500&type=restaurant&keyword=cafe&key=AIzaSyCYAYnOOBhka5cjoRiMYY_f-gzwbKy1eu4')
+    fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + startPos.coords.latitude + ',' + startPos.coords.longitude + '&radius=1500&type=restaurant&key=AIzaSyCYAYnOOBhka5cjoRiMYY_f-gzwbKy1eu4')
     .then(function(response) {
       return response.json();
     })
     .then(function(myJson) {
       var randomNumber = Math.floor(Math.random() * (myJson.results.length))
-      var photoId = myJson.results[randomNumber].photos[0].photo_reference;
-
-      fetch('https://maps.googleapis.com/maps/api/place/photo?maxwidth=1080&photoreference=' + photoId + '&key=AIzaSyCYAYnOOBhka5cjoRiMYY_f-gzwbKy1eu4')
+      console.log(myJson.results.length);
+      console.log(randomNumber);
+      var place = myJson.results[randomNumber];
+      var photoId = place.photos[0].photo_reference;
+      document.getElementById("label").innerHTML = place.name;
+      fetch('https://maps.googleapis.com/maps/api/place/photo?maxwidth=1800&photoreference=' + photoId + '&key=AIzaSyCYAYnOOBhka5cjoRiMYY_f-gzwbKy1eu4')
       .then(response => response.blob())
       .then(images => {
         var outside =  URL.createObjectURL(images);
         document.getElementById("image").src=outside;
+        document.getElementById("image").style.opacity = "1";
       })
 
     });
